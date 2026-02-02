4911f723b6e939fb53c80e142ad0ce751ac771be
mfingcoo <aroberts@everydamndayfitness.com>
2026-02-01 18:20:12 -0600

    Chore: Save uncommitted local changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,26 @@ const config: CapacitorConfig = {
   appId: 'app.hungerfreeandhappy.mobile',
   appName: 'Hunger Free and Happy',
   webDir: 'out',
-  server: {
-    url: 'https://hungerfreeandhappy.app',
-    cleartext: true
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#212121",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    "Assets": {
+      // Temporarily removing the logo to solve the error.
+      // We will only generate the splash screen for now.
+      "splash": {
+        "src": "assets/splash.png",
+        "width": 2732,
+        "height": 2732
+      }
+    }
   }
 };
 
