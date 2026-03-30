122520dd33e42ee27225573e5674b1b09a290956
412rednek <412rednek@gmail.com>
2026-03-30 22:05:21 +0000

    please dear lord let it work

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,6 +12,13 @@ const config: CapacitorConfig = {
     AdMob: {
       appId: 'ca-app-pub-1031680789597179~5170897471',
     },
+    // This is the critical missing piece. 
+    // This configuration tells the native Android and iOS operating systems
+    // what to do with a push notification when the app is in the background or closed.
+    // Without this, the OS has no instruction to show an alert, and silently discards the notification.
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
   },
 };
 
